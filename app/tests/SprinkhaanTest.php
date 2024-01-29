8f7d988892745a8bae7e77cdfb8b97f04fb8abbe
maaike <maaikeverloop@gmail.com>
2024-01-29 15:16:37 +0100

    Sprinkhaan test regel 1

---
+++ app/tests/SprinkhaanTest.php
@@ -0,0 +1,39 @@
+<?php
+
+class SprinkhaanTest extends PHPUnit\Framework\TestCase
+{
+    //todo regels implementeren
+
+    //todo regel 1: Een sprinkhaan verplaatst zich door in een
+    // rechte lijn een sprong te maken naar een veld meteen
+    // achter een andere steen in de richting van de sprong
+
+    public function testWhenSprinkhaanMovesThenItMovesInAStraightLine() {
+        $fromPosition = '(0,0)';
+        $toPosition = '(0,2)';
+
+        $sprinkhaan = new Sprinkhaan($fromPosition);
+        $sprinkhaan->move($toPosition);
+
+        $result = $sprinkhaan->getPosition();
+        $expectedPosition = ('0,2');
+        self::assertEquals($expectedPosition, $result);
+    }
+
+
+    //todo regel 2: Een sprinkhaan mag zich niet verplaatsen
+    // naar het veld waar hij al staat
+
+    //todo regel 3: Een sprinkhaan moet over minimaal 1 steen springen
+
+    //todo regel 4: Een sprinkhaan mag niet naar een bezet veld springen
+
+    //todo regel 5: Een sprinkhaan mag niet over lege velden springen.
+    // dit betekent dat alle velden tussen de start- en eindpositie
+    // bezet moeten zijn
+
+
+
+
+
+}
