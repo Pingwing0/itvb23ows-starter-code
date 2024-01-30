a7e334c3f4b5f090e5305ade9c5abaa5dc9e699e
maaike <maaikeverloop@gmail.com>
2024-01-30 15:04:14 +0100

    Soldatenmier - move once implementatie + kleine testaanpassing

---
+++ app/src/pieces/Soldatenmier.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\pieces;
+
+use app\Board;
+use app\Player;
+use app\RulesException;
+
+class Soldatenmier extends Piece
+{
+
+    /**
+     * @throws RulesException
+     */
+    public function moveOnce($board, $toPosition) {
+        if ($this->slideOneSpace($board, $this->position, $toPosition)) {
+            return $toPosition;
+        } else {
+            throw new RulesException("Ant can't move more than 1 tile at once");
+        }
+    }
+
+    public function slideOneSpace(Board $board, $from, $to): bool
+    {
+        $boardTiles = $board->getBoardTiles();
+        unset($boardTiles[$from]);
+
+        return $board->pieceHasNeighbour($boardTiles, $to) &&
+            $board->pieceIsNeighbourOf($from, $to) &&
+            self::onlyOneTileDifference($from, $to);
+    }
+
+    public function onlyOneTileDifference($from, $to): bool {
+        $fromArray = explode(",", $from);
+        $toArray = explode(",", $to);
+
+        return (abs($fromArray[0] - $toArray[0]) == 1 && $fromArray[1] == $toArray[1]) ||
+            (abs($fromArray[1] - $toArray[1]) == 1 && $fromArray[0] == $toArray[0]) ||
+            ($fromArray[0] - $toArray[0] == -1 && $fromArray[1] - $toArray[1] == 1) ||
+            ($fromArray[0] - $toArray[0] == 1 && $fromArray[1] - $toArray[1] == -1);
+    }
+
+
+    public function moveIsLegal(Board $board, Player $player, string $fromPosition, string $toPosition)
+    {
+        // TODO: Implement moveIsLegal() method.
+    }
+}
--- app/tests/SoldatenmierTest.php
+++ app/tests/SoldatenmierTest.php
@@ -1,17 +1,26 @@
 <?php
 
+use app\pieces\Soldatenmier;
+
 class SoldatenmierTest extends PHPUnit\Framework\TestCase
 {
     //todo regel 2
     // Een verschuiving is een zet zoals de bijenkoningin die mag maken.
     public function testWhenSoldatenmierMovesOnceThenItMovesToANeighbouringField() {
-        $fromPosition = '0,0';
-        $toPosition = '-1,0';
+        $fromPosition = '-1,0';
+        $toPosition = '-1,1';
+        $boardTiles = [
+            '0,0' => [[0, "Q"]],
+            '1,0' => [[1, "Q"]],
+            '-1,0' => [[0, "A"]],
+            '2,0' => [[1, "B"]]
+        ];
+        $board = new \app\Board($boardTiles);
 
         $soldatenmier = new Soldatenmier($fromPosition);
 
-        $result = $soldatenmier->getPosition();
-        $expectedResult = '-1,0';
+        $result = $soldatenmier->moveOnce($board, $toPosition);
+        $expectedResult = '-1,1';
         self::assertEquals($expectedResult, $result);
 
     }
@@ -19,11 +28,18 @@ class SoldatenmierTest extends PHPUnit\Framework\TestCase
     public function testGivenMoveToNotNeighbourWhenSoldatenmierMovesOnceThenThrowsException() {
         $fromPosition = '0,0';
         $toPosition = '1,1';
+        $boardTiles = [
+            '0,0' => [[0, "Q"]],
+            '1,0' => [[1, "Q"]],
+            '-1,0' => [[0, "A"]],
+            '2,0' => [[1, "B"]]
+        ];
+        $board = new \app\Board($boardTiles);
 
         $soldatenmier = new Soldatenmier($fromPosition);
 
         $this->expectException(\app\RulesException::class);
-        $soldatenmier>moveOnce($toPosition);
+        $soldatenmier->moveOnce($board, $toPosition);
 
     }
 
