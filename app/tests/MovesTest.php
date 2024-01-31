06a799e51790de584ca6d3904a0289fb4377ba3b
maaike <maaikeverloop@gmail.com>
2024-01-31 16:38:02 +0100

    Pass - added tests for there is a piece able to move

--- app/tests/MovesTest.php
+++ app/tests/MovesTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use app\Board;
 use app\Moves;
 
 class MovesTest extends PHPUnit\Framework\TestCase
@@ -32,4 +33,32 @@ class MovesTest extends PHPUnit\Framework\TestCase
         self::assertEquals($expectedResult, $result);
     }
 
+    //todo can only pass when no other moves can be played
+
+    public function testWhenNoOtherMovesCanBeDonePlayerIsAbleToPassReturnsTrue() {
+
+
+    }
+
+    //todo test is a piece is able to move
+    public function testWhenPlayerCantMoveAnyPieceThenThereIsAPieceAbleToMoveReturnsFalse() {
+        $board = new Board([]);
+        $player = new \app\Player(0, []);
+
+        $result = Moves::thereIsAPieceAbleToMove($board, $player);
+        self::assertFalse($result);
+    }
+
+    public function testWhenPlayerCanMoveAnyPieceThenThereIsAPieceAbleToMoveReturnsTrue() {
+        $boardTiles = [
+            '0,0' => [[0, "Q"]],
+            '0,1' => [[0, "B"]]];
+        $board = new Board($boardTiles);
+        $player = new \app\Player(0, []);
+
+        $result = Moves::thereIsAPieceAbleToMove($board, $player);
+        self::assertTrue($result);
+    }
+
+    //todo test is a piece is able to be played
 }
