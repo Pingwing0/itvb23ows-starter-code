eacfc75c048e5bb1a6dc094fd80958756def2404
maaike <maaikeverloop@gmail.com>
2024-01-31 22:33:59 +0100

    Kever - test can move to occupied tile

---
+++ app/tests/KeverTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use app\Board;
+
+class KeverTest extends PHPUnit\Framework\TestCase
+{
+    //todo test of kan stapelen
+
+    public function testWhenKeverMovesToOccupiedTileThenMoveReturnsTrue() {
+        $boardTiles = [
+            '0,0' => [[0, "B"]],
+            '0,1' => [[1, "B"]],
+            '0,-1' => [[1, "Q"]]];
+        $board = new Board($boardTiles);
+        $fromPosition = '0,1';
+        $toPosition = '0,0';
+        $kever = new Kever($fromPosition);
+
+        $result = $kever->move($board, $fromPosition, $toPosition);
+        self::assertTrue($result);
+
+    }
+}
