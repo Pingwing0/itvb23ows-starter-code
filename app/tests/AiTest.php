861bb3a50813b64545dae74abcea2737ffaa2fdb
maaike <maaikeverloop@gmail.com>
2024-02-01 20:07:46 +0100

    Ai - test correct data to send

--- app/tests/AiTest.php
+++ app/tests/AiTest.php
@@ -2,26 +2,20 @@
 
 use app\ai\Ai;
 use app\ai\CurlRequest;
+use app\Board;
+use app\Player;
 
 class AiTest extends PHPUnit\Framework\TestCase
 {
-    //todo ai mocken
-
     //todo ai implementeren in game
     // voor nu ai = player 2 (black)
     // stappen:
     //  play
     //  move
     //  pass
-    // json sturen
-    // json ontvangen
     // choose player?
-
-    // response is like: [
-    //    "play",
-    //    "Q",
-    //    "-1,-1"
-    //]
+    // when move done -> send to ai
+    // ai -> do move
 
     public function testWhenAiResponseToPostThenResponseIsArray() {
         $curlRequestMock = $this->getMockBuilder(CurlRequest::class)
@@ -38,6 +32,29 @@ class AiTest extends PHPUnit\Framework\TestCase
 
         $result = $ai->postToApi(['postData'], $curlRequestMock);
         $expectedResult = ['play',  'Q', '-1,1'];
+        self::assertEquals($expectedResult, $result);
+    }
+
+    public function testWhenPreparingToSendApiDataThenDataToSendContainsPlayerHandsAndBoard() {
+        $boardTiles = ['0,0' => [[0, "Q"]]];
+        $board = new Board($boardTiles);
+        $playerOne = new Player(0, ['S' => 1]);
+        $playerTwo = new Player(1, ["Q" => 1, "B" => 2]);
+        $currentPlayer = $playerOne;
+
+        $ai = new Ai();
+
+        $result = $ai->getDataToSend($board, $playerOne, $playerTwo, $currentPlayer);
+        $expectedResult = [
+            "move_number" => 0,
+            "hand" => [
+                ['S' => 1],
+                ["Q" => 1, "B" => 2]
+            ],
+            "board" => [
+                '0,0' => [[0, "Q"]]
+            ]
+        ];
         self::assertEquals($expectedResult, $result);
     }
 
